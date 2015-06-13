3d3af29554aec71d003a4e1338dd4ab60b6e777b
kingty <kingty@mofunsky.com>
2015-06-13 15:04:10 +0800

    添加外键关联

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@
 --用户验证信息
 create table user(
 
-	user_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	email varchar(100) not null unique comment '邮箱',
 	password varchar(50) not null comment '密码',
 	register_time date not null comment '注册时间',
@@ -12,7 +12,7 @@ create table user(
 --用户信息
 --还差一个绑定微信号，不知道存什么字段
 create table user_detail(
-	user_detail_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	user_id bigint(20) unsigned not null comment '用户id',
 	username varchar(100) not null comment '用户展示名',
 	status int(1) default 0 comment '用户状态',
@@ -28,7 +28,7 @@ create table user_detail(
 
 --卡片
 create table card(
-	card_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	user_id bigint(20) unsigned not null comment '用户id，这里存主要是为了让card直接能知道user,不再通过卡包去找',
 	url varchar(100)  comment '卡片内容的图片url',
 	title varchar(100) not null comment '卡片标题',
@@ -42,7 +42,7 @@ create table card(
 
 --卡包
 create table package(
-	package_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	user_id bigint(20) unsigned not null comment '用户id',
 	url varchar(100)  comment '卡包封面URL',
 	packagename varchar(100) not null comment '卡包名称',
@@ -53,7 +53,7 @@ create table package(
 
 --卡包和卡片关联表
 create table package_card(
-	package_card_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	card_id bigint(20) unsigned not null comment '卡片id',
 	package_id bigint(20) unsigned not null comment '卡包id',
 	category int(1) default 0 comment '收藏的还是自己建的'
@@ -64,7 +64,7 @@ create table package_card(
 
 --主题卡包
 create table theme(
-	theme_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	url varchar(100)  comment '卡包封面URL',
 	bgurl varchar(100)  comment '卡包背景URL',
 	packagename varchar(100) not null comment '卡包名称',
@@ -75,15 +75,15 @@ create table theme(
 
 --主题卡包和卡片关联表
 create table theme_card(
-	them_card_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	card_id bigint(20) unsigned not null comment '卡片id',
-	theme_package_id bigint(20) unsigned not null comment '主题卡包id',
+	theme_id bigint(20) unsigned not null comment '主题卡包id',
 )engine=innod default charset=utf8 auto_increment=1;
 
 
 --关注关系表
 create table follow(
-	follow_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	follower_id bigint(20) unsigned not null comment '关注人ID',
 	followeder_id bigint(20) unsigned not null comment '被关注人ID',
 	isfollowed int(1) default 0 comment '是否也被它关注，这个字段只要是用于查询好友列表时，不需要再次查找是否被关注，提高查询效率，有待商榷',
@@ -92,7 +92,7 @@ create table follow(
 
 --私信表
 create table message(
-	message_id bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	sender_id bigint(20) unsigned not null comment '发送者id',
 	receiver_id bigint(20) unsigned not null comment '接收者id',
 	card_id bigint(20) unsigned not null default 0 comment '卡片id,如果是送能量石自动产生的推送信息，保存id，默认为0',
@@ -104,13 +104,40 @@ create table message(
 
 --能量石记录表
 create table stone_log(
-	stone_log bigint(20) unsigned not null primary key auto_increment,
+	id bigint(20) unsigned not null primary key auto_increment,
 	user_id bigint(20) unsigned not null comment '用户id',
 	log varchar(100) not null comment '日志信息，例如：送给  xxx 2颗能量石，xxx需要标记高亮' 
 	log_time date not null comment '记录时间',
 	stone_count int(11) default 0 comment '变动的能量石数'，
 	status int(1) default 0 comment '状态'
 )engine=innod default charset=utf8 auto_increment=1;
+
+
+--外键关联
+alter table user_detail add constraint FK_user_detail_user_id foreign key(user_id) REFERENCES user(id)
+
+alter table card add constraint FK_card_user_id foreign key(user_id) REFERENCES user(id)
+
+alter table package add constraint FK_package_user_id foreign key(user_id) REFERENCES user(id)
+
+alter table package_card add constraint FK_package_card_user_id foreign key(card_id) REFERENCES card(id)
+alter table package_card add constraint FK_package_card_package_id foreign key(package_id) REFERENCES package(id)
+
+
+alter table theme_card add constraint FK_theme_card_card_id foreign key(card_id) REFERENCES card(id)
+alter table theme_card add constraint FK_theme_card_theme_id foreign key(theme_id) REFERENCES theme(id)
+
+alter table follow add constraint FK_follow_follower_id foreign key(follower_id) REFERENCES user(id)
+alter table follow add constraint FK_follow_followeder_id foreign key(followeder_id) REFERENCES user(id)
+
+alter table message add constraint FK_message_sender_id foreign key(sender_id) REFERENCES user(id)
+alter table message add constraint FK_message_receiver_id foreign key(receiver_id) REFERENCES user(id)
+alter table message add constraint FK_message_card_id foreign key(card_id) REFERENCES card(id)
+
+alter table stone_log add constraint FK_stone_log_user_id foreign key(user_id) REFERENCES user(id)
+
+
+
 
 
 
